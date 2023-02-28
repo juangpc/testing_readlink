1b1ab9b826b9e81a396d9c311bfc2c41c5e46a94
JuanGPC <juangpc@users.noreply.github.com>
2023-02-28 11:40:42 -0500

    doh8

--- tools/building/script.bat
+++ tools/building/script.bat
@@ -7,17 +7,27 @@ argv=("$@")
 
 function cleanPath()
 {
-    local  cleanPathStr="$(cd "$1" >/dev/null 2>&1; pwd -P)"
+    local  cleanPathStr="$(
+        cd "$1" >/dev/null 2>&1 
+        pwd -P
+    )"
     echo "$cleanPathStr"
 }
+
+absolute_path(){
+    echo "$(cd "$1" >/dev/null 2>&1 ; pwd -P)"
+}
+
 
 BuildFolder=""
 SourceFolder=""
 
 ScriptPath="$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
-BasePath="$(cd "$($ScriptPath)../.." >/dev/null 2>&1; pwd -P)"
-ScriptPath2="$(cleanPath $0)"
-BasePath2="$(cleanPath $($ScriptPath)../..)"
+BasePath="$(cd "$ScriptPath../.." >/dev/null 2>&1; pwd -P)"
+ScriptPath2="$(cleanPath "$(dirname "$0")")"
+BasePath2="$(cleanPath "$ScriptPath2../..")"
+ScriptPath3=$(absolute_path "$(dirname "$0")")
+BasePath3=$(absolute_path "${ScriptPath3}../..")
 
 
 if [ "$(uname)" == "Darwin" ]; then
@@ -35,6 +45,8 @@ doPrintConfiguration() {
   echo " BasePath     = $BasePath"
   echo " ScriptPath2   = $ScriptPath2"
   echo " BasePath2     = $BasePath2"
+  echo " ScriptPath3   = $ScriptPath3"
+  echo " BasePath3     = $BasePath3"
   echo " BuildFolder  = $BuildFolder"
   echo " SourceFolder = $SourceFolder"
   echo " "
@@ -48,8 +60,6 @@ function cleanPath()
     local  cleanPathStr="$(cd "$1" >/dev/null 2>&1; pwd -P)"
     echo "$cleanPathStr"
 }
-
-result=$(myfunc)
 
 BuildFolder=${BasePath}/build/${BuildType}
 SourceFolder=${BasePath}/src
